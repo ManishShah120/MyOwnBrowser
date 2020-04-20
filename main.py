814206b43126f0af7320449fe8d3b218f153ebc0
ManishShah120 <mkshah141@gmail.com>
2020-04-21 01:20:37 +0530

    Final Commit

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import json
 
 from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout,
 				QPushButton, QLabel, QLineEdit, QTabBar,
-				QFrame, QStackedLayout, QTabWidget)
+				QFrame, QStackedLayout, QTabWidget, QShortcut, QKeySequenceEdit, QSplitter)
 
-from PyQt5.QtGui import QIcon, QWindow, QImage
+from PyQt5.QtGui import QIcon, QWindow, QImage, QKeySequence
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
 
@@ -22,19 +22,30 @@ class App(QFrame):
 		super().__init__()
 		self.setWindowTitle("Web Browser")
 
+		self.setBaseSize(1366, 768)
+		self.setMinimumSize(1280, 1024)
 		self.CreateApp()
-		self.setBaseSize(1366,768)
+		self.setWindowIcon(QIcon("logo.png"))
 
 	def CreateApp(self):
 		self.layout = QVBoxLayout()
 		self.layout.setSpacing(0)
 		self.layout.setContentsMargins(0,0,0,0)
 
+		# Create Tabs
 		self.tabbar = QTabBar(movable=True, tabsClosable=True)
 		self.tabbar.tabCloseRequested.connect(self.CloseTab)
 		self.tabbar.tabBarClicked.connect(self.SwitchTab)
-
 		self.tabbar.setCurrentIndex(0)
+		self.tabbar.setDrawBase(False)
+		self.tabbar.setLayoutDirection(Qt.LeftToRight)
+		self.tabbar.setElideMode(Qt.ElideLeft)
+		
+		self.shortcutNewTab = QShortcut(QKeySequence("Ctrl+T"), self)
+		self.shortcutNewTab.activated.connect(self.AddTab)
+		
+		self.shortcutReload = QShortcut(QKeySequence("Ctrl+R"), self)
+		self.shortcutReload.activated.connect(self.ReloadPage)
 		
 		#Kepp track of tabs
 		self.tabCount = 0
@@ -42,16 +53,31 @@ class App(QFrame):
 
 		# Create Addressbar
 		self.Toolbar = QWidget()
+		self.Toolbar.setObjectName("Toolbar")
 		self.ToolbarLayout = QHBoxLayout()
-		self.addressbar = AddressBar()#Done till here
-		
-		self.Toolbar.setLayout(self.ToolbarLayout)
-		self.ToolbarLayout.addWidget(self.addressbar)
-		
-		# New tab button
+		self.addressbar = AddressBar()
 		self.AddTabButton = QPushButton("+")
+
+		# Create AddressBar + button Signals
+		self.addressbar.returnPressed.connect(self.BrowseTo)
 		self.AddTabButton.clicked.connect(self.AddTab)
 		
+		# Set Toolbar Buttons
+		self.BackButton = QPushButton("<")
+		self.BackButton.clicked.connect(self.GoBack)
+		
+		self.ForwardButton = QPushButton(">")
+		self.ForwardButton.clicked.connect(self.GoForward)
+		
+		self.ReloadButton = QPushButton("R")
+		self.ReloadButton.clicked.connect(self.ReloadPage)
+		
+		#Build Toolbar
+		self.Toolbar.setLayout(self.ToolbarLayout)
+		self.ToolbarLayout.addWidget(self.BackButton)
+		self.ToolbarLayout.addWidget(self.ForwardButton)
+		self.ToolbarLayout.addWidget(self.ReloadButton)
+		self.ToolbarLayout.addWidget(self.addressbar)
 		self.ToolbarLayout.addWidget(self.AddTabButton)
 		
 		# Set main View
@@ -59,6 +85,7 @@ class App(QFrame):
 		self.container.layout = QStackedLayout()
 		self.container.setLayout(self.container.layout)
 
+		# Construct main view from top level Elements
 		self.layout.addWidget(self.tabbar)
 		self.layout.addWidget(self.Toolbar)
 		self.layout.addWidget(self.container)
@@ -77,39 +104,142 @@ class App(QFrame):
 		
 		self.tabs.append(QWidget())
 		self.tabs[i].layout = QVBoxLayout()
+		self.tabs[i].layout.setContentsMargins(0,0,0,0)
+		
+		
+		# For tab switching
 		self.tabs[i].setObjectName("tab" + str(i))
 		
-		# Open webview
+		# Create webview within the tabs top level widget
 		self.tabs[i].content = QWebEngineView()
 		self.tabs[i].content.load(QUrl.fromUserInput("http://google.com"))
+
+		#self.tabs[i].content1 = QWebEngineView()
+		#self.tabs[i].content1.load(QUrl.fromUserInput("http://google.com"))
+
+		self.tabs[i].content.titleChanged.connect(lambda: self.SetTabContent(i, "title"))
+		self.tabs[i].content.iconChanged.connect(lambda: self.SetTabContent(i, "icon"))
+		self.tabs[i].content.urlChanged.connect(lambda: self.SetTabContent(i, "url"))
 		
-		#Add web view to tabs layout
-		self.tabs[i].layout.addWidget(self.tabs[i].content)
+		# Add widget to tab.layout
+		self.tabs[i].splitview = QSplitter()
+		self.tabs[i].splitview.setOrientation(Qt.Vertical)
+		self.tabs[i].layout.addWidget(self.tabs[i].splitview)
 		
-		# set top level tab from [] to layout
+		self.tabs[i].splitview.addWidget(self.tabs[i].content)
+		#self.tabs[i].splitview.addWidget(self.tabs[i].content1)
+
+		# Set tablayout to .layout
 		self.tabs[i].setLayout(self.tabs[i].layout)
-		
-		#Add tab to top level stackwidget
+
+		#Add and set new tabs content to the stack widget
 		self.container.layout.addWidget(self.tabs[i])
 		self.container.layout.setCurrentWidget(self.tabs[i])
-		
-		#Set the tab at the top of screen
+
+		# Create tab on tabbar,  representing this tab,
+		#Set tabData to tab <#> So it knows what self.tabs[#] it needs to control
 		self.tabbar.addTab("New Tab")
-		self.tabbar.setTabData(i,"tab" + str(i))
+		self.tabbar.setTabData(i, {"object": "tab" + str(i), "initial": i})
+		
+		
 		self.tabbar.setCurrentIndex(i)
 		
+		# +=1
 		self.tabCount += 1
 
 	def SwitchTab(self, i):
-		tab_data = self.tabbar.tabData(i)
-		print("Tab: ", tab_data)
+		if self.tabbar.tabData(i):
+			tab_data = self.tabbar.tabData(i)["object"]	
+			tab_content = self.findChild(QWidget, tab_data)
+			self.container.layout.setCurrentWidget(tab_content)
+			new_url = tab_content.content.url().toString()
+			self.addressbar.setText(new_url)
+
+	def BrowseTo(self):
+		text = self.addressbar.text()
+		print(text)
+
+		i = self.tabbar.currentIndex()
+		tab = self.tabbar.tabData(i)["object"]
+		wv = self.findChild(QWidget, tab).content
+
+		if "http" not in text:
+			if "." not in text:
+				url = "http://www.google.com/#q=" + text
+			else:
+				url = "http://" + text
+		else:
+			url = text
 		
-		tab_content = self.findChild(QWidget, tab_data)
-		self.container.layout.setCurrentWidget(tab_content)
+		wv.load(QUrl.fromUserInput(text))
+		
+		
+	def SetTabContent(self, i, type):
+		'''
+			self.tabs[i].objectName = tab1
+			self.tabbar.tabData(i)["object"] = tab1
+		'''		
+		tab_name = self.tabs[i].objectName()
+		# tab1
+
+		count = 0
+		running = True
+		
+		current_tab = self.tabbar.tabData(self.tabbar.currentIndex())["object"]
+		
+		if current_tab ==  tab_name and type == "url":
+			new_url = self.findChild(QWidget, tab_name).content.url().toString()
+			self.addressbar.setText(new_url)
+			return False
+
+		while running:
+			tab_data_name = self.tabbar.tabData(count)
+
+			if count >= 99:
+				running = False
+				
+			if tab_name == tab_data_name["object"]:
+				if type == "title":
+					newTitle = self.findChild(QWidget, tab_name).content.title()
+					self.tabbar.setTabText(count, newTitle)
+				elif type == "icon":
+					newIcon = self.findChild(QWidget, tab_name).content.icon()
+					self.tabbar.setTabIcon(count, newIcon)
+					
+				running = False
+			else:
+				count += 1
+				
+	def GoBack(self):
+		activeIndex = self.tabbar.currentIndex()
+		tab_name = self.tabbar.tabData(activeIndex)["object"]
+		tab_content = self.findChild(QWidget, tab_name).content
+		
+		tab_content.back()
+		
+	def GoForward(self):
+		activeIndex = self.tabbar.currentIndex()
+		tab_name = self.tabbar.tabData(activeIndex)["object"]
+		tab_content = self.findChild(QWidget, tab_name).content
+		
+		tab_content.forward()
+		
+	def ReloadPage(self):
+		activeIndex = self.tabbar.currentIndex()
+		tab_name = self.tabbar.tabData(activeIndex)["object"]
+		tab_content = self.findChild(QWidget, tab_name).content
+		
+		tab_content.reload()
+
 
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
+	#os.environ['QTWEBENGINE_REMOTE_DEBUGGING'] = "667"
+	
 	window = App()
+	
+	with open("style.css", "r") as style:
+		app.setStyleSheet(style.read())
 
 	sys.exit(app.exec_())
